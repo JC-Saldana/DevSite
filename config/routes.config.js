d1ad7d4fdf8d785054a833ec17bf7514dfb11de2
Pablo Mateos <pmateos805@gmail.com>
2022-03-02 19:13:15 +0100

    Active social users & input files

--- config/routes.config.js
+++ config/routes.config.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const passport = require('passport')
-const upload = require('../config/storage.config');
+
+const upload = require('../config/storage.config')
 
 //scopes for google login
 const SCOPES = [
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -9,6 +9,9 @@ module.exports.edit = (req, res, next) => {
 }
 
 module.exports.doEdit = (req, res, next) => {
+    if (req.file) {
+        req.body.avatar = req.file.path
+    }
     User.findByIdAndUpdate(req.params.id, req.body, { runValidators: true, new: true })
         .then((user) => res.redirect(`/user/${user.id}`))
         .catch(next)
